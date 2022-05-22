66fd1c44c3ab062a639bd28a4319fbf790bb8be7
Abdelali KAMIL <kamilabdelali.1@gmail.com>
2022-05-22 04:10:20 +0100

    Commit 202205220410

--- views/header.php
+++ views/header.php
@@ -1,13 +1,15 @@
-<header>
+<header class="header">
     <nav>
         <div class="row">
-            <img src="https://www.seekpng.com/png/full/90-904256_instant-rockstar-r-logo-red-r-logo-hd.png" alt="Rebrancy Logo" class="logo">
+            <div class="logo">
+                <a href="#"><img src="/rebrancy/public/img/RebrancyYellow.png" alt="Rebrancy Logo"></a>
+            </div>
             <ul class="main-nav">
-                <li><a class="nav-link" href="#">Brands</a></li>
-                <li><a class="nav-link" href="#">Influencers</a></li>
-                <li><a class="nav-link" href="#">Business</a></li>
-                <li class="nav-item sign-in"><a class="nav-link" href="#">Sign in</a></li>
-                <li class="nav-item sign-up"><a class="nav-link" href="#">Sign up</a></li>
+                <li><a class="nav-link" href="#">Marques</a></li>
+                <li><a class="nav-link" href="#">Influenceurs</a></li>
+                <li><a class="nav-link" href="#">Partenariat</a></li>
+                <li class="nav-item sign-in"><a class="nav-link" href="#">Se connecter</a></li>
+                <li class="nav-item sign-up"><a class="nav-link" href="#">S'inscrire</a></li>
             </ul>
         </div>
     </nav>
--- views/home/index.php
+++ views/home/index.php
@@ -16,26 +16,27 @@
         </div>
     </div>
 </section>
-<section class="section">
+<section class="section about">
     <div class="row">
-        <div class="col span-1-of-2">
+        <div class="about-container">
             <div class="about-text">
-                <h2>Un écosystème où les marques et les influenceurs se rencontrent</h2>
-                <p>Je suis une marque</p>
+                <h2 class="text-primary">Collaborez avec les meilleures marques</h2>
+                <p>Rebrancy aide les influenceurs Instagram et YouTube à s'associer avec des marques leaders pour recevoir des produits gratuits en échange d'un contenu authentique.</p>
+                <div class="about-btn">
+                    <a class="btn btn-lg-secondary" href="#">Commencer maintenant</a>
+                </div>
             </div>
-        </div>
-        <div class="col span-2-of-2">
             <div class="about-image">
                 <img src="https://images.unsplash.com/photo-1543269865-cbf427effbad?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="About Image">
             </div>
         </div>
     </div>
 </section>
-<section class=" top-brands">
+<section class="section top-brands">
     <div class="row">
         <div class="col span-2-of-2 text-center">
-            <h2 class="text-primary">Connect with brands</h2>
-            <h5 class="section-subtitle">Rebrancy's platform helps influencers connect with brands for free products, sponsorships, paid campaigns, and more.</p>
+            <h2 class="text-secondary">Connect with brands</h2>
+            <h5 class="section-subtitle text-white">Rebrancy's platform helps influencers connect with brands for free products, sponsorships, paid campaigns, and more.</p>
         </div>
     </div>
     <div class="row">
@@ -43,15 +44,33 @@
             <ul class="cards">
                 <li>
                     <a href="" class="card">
-                        <img src="https://images.unsplash.com/photo-1594971475674-6a97f8fe8c2b?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774" class="card__image" alt="" />
+                        <img src="https://images.unsplash.com/photo-1585898643467-fe395370b450?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNvY2ElMjBjb2xhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600" class="card__image" alt="" />
                         <div class="card__overlay">
                             <div class="card__header">
                                 <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
                                     <path />
                                 </svg>
-                                <img class="card__thumb" src="https://seeklogo.com/images/C/coca-cola-circle-logo-A9EBD3B00A-seeklogo.com.png" alt="" />
+                                <img class="card__thumb" src="https://www.svgrepo.com/show/303132/coca-cola-logo.svg" alt="" />
                                 <div class="card__header-text">
                                     <h5 class="card__title">Coca Cola</h5>
+                                    <span class="card__status">Beverage Company</span>
+                                </div>
+                            </div>
+                            <p class="card__description">La société Coca-Cola, officiellement nommée The Coca-Cola Company of Atlanta, est une entreprise américaine spécialisée dans les boissons non alcoolisées. Il s'agit de l'une des plus importantes sociétés américaines dont la notoriété et la popularité sont mondiales</p>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="card">
+                        <img src="https://images.unsplash.com/photo-1566796201787-b034938b4396?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464" class="card__image" alt="" />
+                        <div class="card__overlay">
+                            <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                                    <path />
+                                </svg>
+                                <img class="card__thumb" src="http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c4f2.png" alt="" />
+                                <div class="card__header-text">
+                                    <h5 class="card__title">Adidas</h5>
                                     <span class="card__status">Beverage Company</span>
                                 </div>
                             </div>
@@ -61,16 +80,16 @@
                 </li>
                 <li>
                     <a href="" class="card">
-                        <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+                        <img src="https://images.unsplash.com/photo-1637851682487-fa13fce5150f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGtmY3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600" class="card__image" alt="" />
                         <div class="card__overlay">
                             <div class="card__header">
                                 <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
                                     <path />
                                 </svg>
-                                <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+                                <img class="card__thumb" src="https://www.pngplay.com/wp-content/uploads/9/KFC-Logo-PNG-Background.png" alt="" />
                                 <div class="card__header-text">
-                                    <h3 class="card__title">Jessica Parker</h3>
-                                    <span class="card__status">1 hour ago</span>
+                                    <h5 class="card__title">KFC</h5>
+                                    <span class="card__status">Beverage Company</span>
                                 </div>
                             </div>
                             <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
@@ -79,16 +98,16 @@
                 </li>
                 <li>
                     <a href="" class="card">
-                        <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+                        <img src="https://images.unsplash.com/photo-1616790151040-47661836dd26?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fG1lcmNlZGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600" class="card__image" alt="" />
                         <div class="card__overlay">
                             <div class="card__header">
                                 <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
                                     <path />
                                 </svg>
-                                <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+                                <img class="card__thumb" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Mercedes-Benz_Star_2022.svg/1200px-Mercedes-Benz_Star_2022.svg.png" alt="" />
                                 <div class="card__header-text">
-                                    <h3 class="card__title">Jessica Parker</h3>
-                                    <span class="card__status">1 hour ago</span>
+                                    <h5 class="card__title">Mercedes</h5>
+                                    <span class="card__status">Beverage Company</span>
                                 </div>
                             </div>
                             <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
@@ -97,16 +116,16 @@
                 </li>
                 <li>
                     <a href="" class="card">
-                        <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+                        <img src="https://images.unsplash.com/photo-1548364538-60b952c308b9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJyYW5kc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600" class="card__image" alt="" />
                         <div class="card__overlay">
                             <div class="card__header">
                                 <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
                                     <path />
                                 </svg>
-                                <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+                                <img class="card__thumb" src="https://www.designbust.com/download/1031/png/starbucks_logo_transparent512.png" alt="" />
                                 <div class="card__header-text">
-                                    <h3 class="card__title">Jessica Parker</h3>
-                                    <span class="card__status">1 hour ago</span>
+                                    <h5 class="card__title">Starbucks</h5>
+                                    <span class="card__status">Beverage Company</span>
                                 </div>
                             </div>
                             <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
@@ -115,53 +134,36 @@
                 </li>
                 <li>
                     <a href="" class="card">
-                        <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+                        <img src="https://images.unsplash.com/photo-1563170423-741125661ae8?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzl8fHJheWJhbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600" class="card__image" alt="" />
                         <div class="card__overlay">
                             <div class="card__header">
                                 <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
                                     <path />
                                 </svg>
-                                <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+                                <img class="card__thumb" src="https://cdn.freebiesupply.com/logos/thumbs/2x/ray-ban-logo.png" alt="" />
                                 <div class="card__header-text">
-                                    <h4 class="card__title">Jessica Parker</h4>
-                                    <span class="card__status">1 hour ago</span>
+                                    <h5 class="card__title">Rayban</h5>
+                                    <span class="card__status">Beverage Company</span>
                                 </div>
                             </div>
                             <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
                         </div>
                     </a>
                 </li>
-                <li>
-                    <a href="" class="card">
-                        <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-                        <div class="card__overlay">
-                            <div class="card__header">
-                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                                    <path />
-                                </svg>
-                                <img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
-                                <div class="card__header-text">
-                                    <h3 class="card__title">kim Cattrall</h3>
-                                    <span class="card__status">3 hours ago</span>
-                                </div>
-                            </div>
-                            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                        </div>
-                    </a>
-                </li>
+            </ul>
         </div>
     </div>
     <div class="row">
         <div class="col span-2-of-2 text-center">
-            <a class="btn btn--primary" href="../brand/brandList.php?action=showBrands">Voir plus</a>
+            <a class="btn btn-light" href="../brand/brandList.php?action=showBrands">Voir plus</a>
         </div>
     </div>
 </section>
 <section class="section top-influencers">
     <div class="row">
         <div class="col span-2-of-2 text-center">
-            <h2 class="text-secondary">Connect with influencers</h2>
-            <h5 class="text-white">Outreach influencers to develop credibility and raise awareness for your brand or product</p>
+            <h2 class="text-primary">Connect with influencers</h2>
+            <h5 class="section-subtitle">Outreach influencers to develop credibility and raise awareness for your brand or product</p>
         </div>
     </div>
     <div class="row">
@@ -219,7 +221,7 @@
         </div>
         <div class="row">
             <div class="col span-2-of-2 text-center">
-                <a class="btn btn--primary" href="../brand/brandList.php?action=showInfluencers">Voir plus</a>
+                <a class="btn btn-secondary" href="../brand/brandList.php?action=showInfluencers">Voir plus</a>
             </div>
         </div>
 </section>
